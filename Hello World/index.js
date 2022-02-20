06af9f3027995569f50d252afdc55381e2663c2a
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2022-02-20 12:17:46 -0400

    Added files belonging to a "Hello World"-style Project

---
+++ Hello World/index.js
@@ -0,0 +1,21 @@
+//import React from "react"
+//import ReactDOM from "react-dom"
+
+
+const page = (
+    <div>
+        <h1>Japanese Car Manufacturers</h1>
+        <h3>World's most renowned and reliable</h3>
+        <ol>
+            <li>Mazda</li>
+            <li>Toyota</li>
+            <li>Lexus</li>
+            <li>Honda</li>
+            <li>Nissan</li>
+
+        </ol>
+    </div>            
+)
+
+//document.getElementById("root").append(JSON.stringify(page))
+ReactDOM.render(page, document.getElementById("root"))
